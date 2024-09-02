9b0964cf859c66925bb00c47de74863149ce659c
Melissa Wang <melissa.wang21@gmail.com>
2024-09-02 17:56:29 -0400

    more efficient target sum solution

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
-  const len = array.length
-  for (i = 0; i < len - 1; i++) {
-    if (array[i] < target) {
-      for (j = i + 1; j < len; j++) {
-        if (array[i] + array[j] == target) {
-          return true;
-        }
-      }
-    } 
+  // create an object to keep track of all the numbers seen previously
+  const seenNums = {};
+  for (const num of array) {
+    // for the current num, find the complementary num that adds up to the target
+    const comp = target - num;
+    // check to see if the complement has been seen in the array
+    // if it has, then two numbers exist that add up to the target, return true
+    if (seenNums[comp]) return true
+    seenNums[num] = true;
   }
+  // if the end of the array is reached, return false
   return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
-  The time complexity of this algorithm is O(n * (n - 1)), or O(n^2)
+  The time complexity of this algorithm is O(n)
 */
 
 /* 
   Add your pseudocode here
-  For each element of the array, if the element is less than the target, check the remaining
-  elements to see if they add up to the target. If they do, return true.
-  Otherwise, return false.
+  For each number in the array, store it in an object and check to see if a number in the
+  array has been seen before that adds up to the target (the complement). If the complement has
+  been seen before, return true. Otherwise, move on the next element. If the end of the array
+  has been reached, than none of the array numbers have complements in the array that add up to
+  target, return false.
 */
 
 /*
   Add written explanation of your solution here
-  If the element in the array is larger than the target, there is no need to check the other
-  elements because there is no number in the array that can be added to achieve the target value.
-  If the element is smaller, then the remaining elements must be checked in order to see if any of
-  them add up to the target value. The last element does not need to be checked because it was
-  already attempted to be added to the previous elements. Once the array has been iterated through,
-  then it returns false.
+  For numbers in an array to add up to the target value, it must have a complementary number
+  in the array (target - number). As the array is iterated through, the value is stored for 
+  comparison for future elements. If a number's complement exist, it will be stored in an object
+  as a key. Otherwise, no two numbers exist that add up to the target.
 */
 
 // You can run `node index.js` to view these console logs
